9041d42eaa1163b1537b49cc045aa24cfa5bb8ad
Ranranruo <tlsalstjr58@gmail.com>
2026-08-23 21:17:20 +0900

    feat: header

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -1,46 +1,57 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { Link, NavLink, Outlet } from "react-router-dom";
+import Logo from "../assets/logo.svg";
 
 const Menus = [
-    { name: "Home", path: "/", },
-    { name: "Games", path: "/games" },
-    { name: "Ranking", path: "/ranking" },
-    { name: "Community", path: "/community" },
-    { name: "Creator", path: "/creator" }
+    { name: "홈", path: "/", },
+    { name: "게임", path: "/games" },
+    { name: "랭킹", path: "/ranking" },
+    { name: "커뮤니티", path: "/community" },
+    { name: "크리에이터", path: "/creator" }
 ]
 
 const BasicLayout = () => {
     return (
         <>
-            <header className="container h-20 flex items-center">
-                <div className="header-left flex gap-12">
-                    <div className="flex gap-1">
-                        <p className="text-xl font-bold text-white">DEMO</p>
-                        <p className="text-xl font-bold text-accent-light">NAMES</p>
+            <header className="flex justify-center items-center h-20 border-b border-neutral-950">
+                <div className="inner container flex items-center justify-between">
+                    <div className="header-left flex gap-12">
+                        <a href="/" className="logo flex gap-1">
+                            <img src={Logo} alt="Logo" />
+                            <p className="text-xl font-bold text-white">LU</p>
+                            <p className="text-xl font-bold text-primary-400">MEN</p>
+                        </a>
+                        <ul className="menu flex gap-7">
+                            {Menus.map((menu) => (
+                                <li>
+                                    <NavLink to={menu.path} className={({isActive})=>`
+                                            text-sm
+                                            ${isActive ? 
+                                                `
+                                                relative
+                                                text-neutral-50
+                                                after:content-[''] after:absolute after:bg-primary-500 after:rounded-full after:size-[5px] after:left-1/2 after:-translate-x-1/2 after:bottom-[-15px] 
+                                                `:
+                                                `text-neutral-600 hover:text-neutral-50 transition duration-300`
+                                            }
+                                        `}>
+                                        {menu.name}
+                                    </NavLink>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
-                    <ul className="menu flex gap-7">
-                        {Menus.map((menu) => (
-                            <li>
-                                <NavLink to={menu.path} className={({isActive})=>`
-                                        text-sm
-                                        ${isActive ? 
-                                            `
-                                            relative
-                                            text-white
-                                            after:content-[''] after:absolute after:bg-accent after:rounded-b-full after:size-[5px] after:left-1/2 after:-translate-x-1/2 after:bottom-[-13px] 
-                                            `:
-                                            `text-muted hover:text-white transition duration-300`
-                                        }
-                                    `}>
-                                    {menu.name}
-                                </NavLink>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                <div className="header-right">
-                    <div className="search flex gap-1">
-                        <div className="search-icon ">⌕</div>
-                        <input type="text" placeholder="게임, 장르 또는 크리에이터 검색" />
+                    <div className="header-right flex items-center gap-2.5">
+                        <div className="search flex border transition w-[245px] h-[38px] items-center gap-2.5 px-4 border-neutral-900  bg-neutral-950 rounded-sm duration-300 hover:border-primary-500 focus-within:border-primary-500">
+                            <div className="search-icon text-sm text-neutral-600">⌕</div>
+                            <input className="search-input w-full  text-neutral-50 placeholder:text-neutral-600 text-xs" type="text" placeholder="게임, 장르 또는 크리에이터 검색" />
+                        </div>
+                        <button className="notification flex border transition w-[38px] h-[38px] text-base items-center justify-center rounded-sm text-neutral-400 bg-neutral-950 border-neutral-900 duration-200 hover:brightness-150">
+                            🕭
+                        </button>
+                        {/* <div className="profile flex items-center px-2">
+                            <img className="border cursor-pointer w-[32px] h-[32px] border-neutral-900 rounded-full" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="profile" />
+                        </div> */}
+                        <Link to="/sign-in" className="text-sm">로그인</Link>
                     </div>
                 </div>
             </header>
--- src/routers/Router.tsx
+++ src/routers/Router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import HomePage from "../pages/HomePage";
 import BasicLayout from "../layouts/BasicLayout";
+import SignInPage from "../pages/auth/SignInPage";
 
 const Router = () => {
     return (
@@ -8,6 +9,9 @@ const Router = () => {
             <Routes>
                 <Route element={<BasicLayout/>}>
                     <Route path="/" element={<HomePage/>} />
+                </Route>
+                <Route>
+                    <Route path="sign-in" element={<SignInPage/>} />
                 </Route>
             </Routes>
         </BrowserRouter>
